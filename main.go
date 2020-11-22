6651d2c029fb112aa3bb1dc4f8599e1400fe296f
Winston Yeung <yeung.winston@gmail.com>
2020-11-22 14:42:01 -0800

    Implement InsertListItem API

--- main.go
+++ main.go
@@ -32,6 +32,11 @@ type InsertListOutput struct {
 	ListID string
 }
 
+type InsertListItemInput struct {
+	ItemID  string
+	Content string
+}
+
 func writeInternalErrorResponse(w http.ResponseWriter) {
 	w.WriteHeader(http.StatusInternalServerError)
 	w.Write([]byte("Sorry! Something went wrong"))
@@ -139,11 +144,108 @@ func (s *server) InsertList(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func (s *server) InsertListItem(w http.ResponseWriter, r *http.Request) {
+	//TODO: add validation to inputs
+
+	b, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		log.Errorln(err)
+		writeInternalErrorResponse(w)
+		return
+	}
+
+	var in InsertListItemInput
+	err = json.Unmarshal(b, &in)
+	if err != nil {
+		log.Errorln(err)
+		writeInternalErrorResponse(w)
+		return
+	}
+
+	uid, ok := r.Header["User"]
+	if !ok {
+		log.Errorln("UserId not provided")
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte("UserId is missing"))
+		return
+	}
+	v := mux.Vars(r)
+
+	// TODO: assert input lengths
+	item := model.YataItem{
+		UserID:  model.UserID(uid[0]),
+		ListID:  model.ListID(v["listID"]),
+		ItemID:  model.ItemID(in.ItemID),
+		Content: in.Content,
+	}
+
+	// insert list to db here
+	err = s.ydb.InsertItem(item)
+	if err != nil {
+		log.Errorln(err)
+		writeInternalErrorResponse(w)
+	}
+
+	w.WriteHeader(http.StatusCreated)
+	_, err = w.Write([]byte{})
+	if err != nil {
+		log.Errorln(err)
+	}
+}
+
+func (s *server) GetListItems(w http.ResponseWriter, r *http.Request) {
+	v := mux.Vars(r)
+
+	userID := model.UserID(r.Header.Get("User"))
+	listID := model.ListID(v["listID"])
+
+	items, err := s.ydb.GetListItems(userID, listID)
+	if err != nil {
+		log.Errorln(err)
+		writeInternalErrorResponse(w)
+	}
+
+	res, err := json.Marshal(items)
+	if err != nil {
+		log.Println(err)
+		writeInternalErrorResponse(w)
+	}
+
+	_, err = w.Write(res)
+	if err != nil {
+		log.Println(err)
+	}
+}
+
+func (s *server) GetAllItems(w http.ResponseWriter, r *http.Request) {
+	userID := model.UserID(r.Header.Get("User"))
+
+	items, err := s.ydb.GetAllItems(userID)
+	if err != nil {
+		log.Errorln(err)
+		writeInternalErrorResponse(w)
+	}
+
+	res, err := json.Marshal(items)
+	if err != nil {
+		log.Println(err)
+		writeInternalErrorResponse(w)
+	}
+
+	_, err = w.Write(res)
+	if err != nil {
+		log.Println(err)
+	}
+}
+
 func (s *server) start() {
 	r := mux.NewRouter()
+	r.HandleFunc("/items", s.GetAllItems).Methods(http.MethodGet)
 	r.HandleFunc("/lists", s.GetLists).Methods(http.MethodGet)
 	r.HandleFunc("/lists", s.InsertList).Methods(http.MethodPut)
 	r.HandleFunc("/lists/{listID}/", s.GetList).Methods(http.MethodGet)
+	r.HandleFunc("/lists/{listID}/items", s.GetListItems).Methods(http.MethodGet)
+	r.HandleFunc("/lists/{listID}/items", s.InsertListItem).Methods(http.MethodPut)
 	log.Fatal(http.ListenAndServe(":8888", r))
 }
 
